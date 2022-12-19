6f4b060ac0305a87f94cc24b8041f335e644997e
Choiyu330 <jenny4063@naver.com>
2022-12-20 01:36:33 +0900

    Add Getter/Setter Method

--- src/main/java/com/codestaes/soloproject/dto/TodoPatchDto.java
+++ src/main/java/com/codestaes/soloproject/dto/TodoPatchDto.java
@@ -6,22 +6,23 @@ public class TodoPatchDto {
     private long todoId;
     @NotBlank(message = "수정하려는 내용은 공백이 아니어야 합니다.")
     private String title;
+    private int todo_order;
     private boolean completed;
 
-    public long getTodoId() {
-        return todoId;
-    }
-
+    public long getTodoId() { return todoId; }
     public void setTodoId(long todoId) {
         this.todoId = todoId;
     }
+
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
+
+    public int getTodo_order() { return todo_order; }
+    public void setTodo_order(int todo_order) { this.todo_order = todo_order; }
 
     public boolean getCompleted() {
         return completed;
