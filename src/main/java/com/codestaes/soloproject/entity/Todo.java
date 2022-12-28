345afb5f1ff43dbec151bf5d6bfa0dab0bd457a3
Choiyu330 <jenny4063@naver.com>
2022-12-28 23:55:12 +0900

    feat : Todo Entity Modify

--- src/main/java/com/codestaes/soloproject/entity/Todo.java
+++ src/main/java/com/codestaes/soloproject/entity/Todo.java
@@ -3,18 +3,22 @@ package com.codestaes.soloproject.entity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-
 import javax.persistence.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
+@Entity
 public class Todo {
+    @Id
+    @GeneratedValue
     private long todoId;
     private String title;
     private int todo_order;
     private boolean completed;
+
+    public Todo(String title) {
+        this.title = title;
+    }
 
 }
