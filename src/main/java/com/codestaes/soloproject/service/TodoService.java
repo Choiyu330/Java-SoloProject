1bea152c5f259e38a7b073d8c292f8937f9d9003
Choiyu330 <jenny4063@naver.com>
2022-12-30 23:36:26 +0900

    feat : TodoService 클래스 생성

--- src/main/java/com/codestaes/soloproject/service/TodoService.java
+++ src/main/java/com/codestaes/soloproject/service/TodoService.java
@@ -1,36 +1,4 @@
 package com.codestaes.soloproject.service;
 
-import com.codestaes.soloproject.entity.Todo;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
 public class TodoService {
-    public Todo createTodo(Todo todo) {
-        Todo createdTodo = todo;
-        return createdTodo;
-    }
-
-    public Todo updateTodo(Todo todo) {
-        Todo updatedTodo = todo;
-        return updatedTodo;
-    }
-
-    public Todo findTodo(long todoId) {
-        Todo todo = new Todo(todoId, "뭐하지", 1, false);
-        return todo;
-    }
-
-    public List<Todo> findTodos() {
-        List<Todo> todos = List.of(
-                new Todo(1, "첫 번째 할 일", 1, true),
-                new Todo(2, "두 번째 할 일", 2, false)
-        );
-        return todos;
-    }
-
-    public void deleteTodo(long todoId) {
-
-    }
 }
