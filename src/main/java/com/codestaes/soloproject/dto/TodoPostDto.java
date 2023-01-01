0b2bc136f477723b57512a946bee569cf73aa6f9
Choiyu330 <jenny4063@naver.com>
2023-01-01 23:59:12 +0900

    feat : TodoPostDto 수정

--- src/main/java/com/codestaes/soloproject/dto/TodoPostDto.java
+++ src/main/java/com/codestaes/soloproject/dto/TodoPostDto.java
@@ -1,35 +1,21 @@
 package com.codestaes.soloproject.dto;
 
-import javax.validation.constraints.NotBlank;
+import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
 public class TodoPostDto {
 
+    @Positive
     @NotBlank(message = "내용은 공백이 아니어야 합니다.")
     private String title;
     private int todo_order;
     private boolean completed;
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getTodo_order() {
-        return todo_order;
-    }
-
-    public void setTodo_order(int todo_order) {
-        this.todo_order = todo_order;
-    }
-
-    public boolean getCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
 }
