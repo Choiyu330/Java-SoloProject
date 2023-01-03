4acf74e2242be24930f92ab8bbc65258ea2124e5
Choiyu330 <jenny4063@naver.com>
2023-01-03 18:04:40 +0900

    feat : TodoPostDto / TodoPatchDto 수정

--- src/main/java/com/codestaes/soloproject/dto/TodoPatchDto.java
+++ src/main/java/com/codestaes/soloproject/dto/TodoPatchDto.java
@@ -1,34 +1,19 @@
 package com.codestaes.soloproject.dto;
 
+import lombok.*;
+
 import javax.validation.constraints.NotBlank;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
 public class TodoPatchDto {
-    private long todoId;
+    private Long todoId;
     @NotBlank(message = "수정하려는 내용은 공백이 아니어야 합니다.")
     private String title;
     private int todo_order;
-    private boolean completed;
+    private Boolean completed;
 
-    public long getTodoId() { return todoId; }
-    public void setTodoId(long todoId) {
-        this.todoId = todoId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getTodo_order() { return todo_order; }
-    public void setTodo_order(int todo_order) { this.todo_order = todo_order; }
-
-    public boolean getCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
 }
--- src/main/java/com/codestaes/soloproject/dto/TodoPostDto.java
+++ src/main/java/com/codestaes/soloproject/dto/TodoPostDto.java
@@ -3,7 +3,6 @@ package com.codestaes.soloproject.dto;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Positive;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,11 +10,9 @@ import javax.validation.constraints.Positive;
 @Getter
 @Setter
 public class TodoPostDto {
-
-    @Positive
     @NotBlank(message = "내용은 공백이 아니어야 합니다.")
     private String title;
     private int todo_order;
-    private boolean completed;
+    private Boolean completed;
 
 }
