1c3ff925de12c6d222469853c617a569b2b02b59
Choiyu330 <jenny4063@naver.com>
2023-01-26 20:44:04 +0900

    refactor: Homecontroller 수정

--- src/main/java/com/codestaes/soloproject/home/Homecontroller.java
+++ src/main/java/com/codestaes/soloproject/home/Homecontroller.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@RestController("/")
+@RestController
 public class Homecontroller {
-    @GetMapping
+    @GetMapping("/")
     public ResponseEntity helloWorld() {
         return ResponseEntity.ok().body(
                 Map.of("hello", "world!")
